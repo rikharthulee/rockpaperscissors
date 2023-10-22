da7882ae0ac96dfb4ac36fe9d09d9c11d6683a01
rikharhthulee <rikharhthulee@gmail.com>
2023-10-22 02:24:45 +0100

    Create function to get computers choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+// Computer picks rock paper or scissors from the array
+
+const options = ['Rock', 'Paper', 'Scissors']; // options in an array
+
+function getComputerChoice() {
+
+    // get random number from 0 to total of items in the array.  If more items are added, they will be automatically included.
+    const randomIndex = Math.floor(Math.random() * options.length);
+
+    // get random choice
+    const choice = options[randomIndex]; 
+
+    return choice;
+}
+
+
+
+console.log(getComputerChoice());
