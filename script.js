f35870da7252e0febfa5b89b9f459e3f8b104dd6
rikharhthulee <rikharhthulee@gmail.com>
2023-10-22 03:07:33 +0100

    Added user promt and case validation

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 // Computer picks rock paper or scissors from the array
 
 const options = ['Rock', 'Paper', 'Scissors']; // options in an array
+let computerSelection;
+let playerSelection;
+let computerScore;
+let playerScore;
 
 function getComputerChoice() {
     return options[~~(Math.random()*options.length)]
+};
+
+computerSelection = getComputerChoice();
+
+
+function playerInput () { //ask user for their selection.  If it is anything other than lowercase, print error message and ask again.
+    playerSelection = prompt("rock, paper or scissors?"); 
+        if ((playerSelection.toLowerCase()=='rock')
+            ||(playerSelection.toLowerCase()=='paper')
+            ||(playerSelection.toLowerCase()=='scissors')) {
+        } else {
+        alert('That\'s not a valid choice.');
+        playerInput();
+        }
 }
 
-const computerSelection = getComputerChoice()
 
-
-
-
-let userSelection = prompt("Rock, Paper or Scissors?"); //ask user for their selection
+playerInput()
 
 console.log(computerSelection);
-console.log(userSelection)
-
+console.log(playerSelection);
+console.log(computerScore);
+console.log(playerScore);
