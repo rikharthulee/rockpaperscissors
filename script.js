38f0626e8057fa32412868616e0f812ce871fbc5
rikharhthulee <rikharhthulee@gmail.com>
2023-10-22 07:42:38 +0100

    added declareResult function

--- script.js
+++ script.js
@@ -1,33 +1,56 @@
 // Computer picks rock paper or scissors from the array
 
 const options = ['Rock', 'Paper', 'Scissors']; // options in an array
-let computerSelection;
-let playerSelection;
+
 let computerScore;
 let playerScore;
 
-function getComputerChoice() {
-    return options[~~(Math.random()*options.length)]
-};
+/* funtion to randomly generate computers choice */
 
-computerSelection = getComputerChoice();
-
-
-function playerInput () { //ask user for their selection.  If it is anything other than lowercase, print error message and ask again.
-    playerSelection = prompt("rock, paper or scissors?"); 
-        if ((playerSelection.toLowerCase()=='rock')
-            ||(playerSelection.toLowerCase()=='paper')
-            ||(playerSelection.toLowerCase()=='scissors')) {
-        } else {
-        alert('That\'s not a valid choice.');
-        playerInput();
-        }
+function getComputerChoice(computerSelection) {
+    return computerSelection = options[~~(Math.random()*options.length)]
 }
 
+/* Funtion to obtain user input
+Asks user for their choice.  If it is anything other than rock, paper or scissors print error message and ask again.  
+tolowercase changes the input to lowercase for validation but it will still output whatever the user wrote, i.e. Rock rock or ROCK.
+*/
 
-playerInput()
+function getPlayerInput() {
+    playerSelection = prompt("rock, paper or scissors?"); 
+        if ((playerSelection.toLowerCase()=='rock')
+          ||(playerSelection.toLowerCase()=='paper')
+          ||(playerSelection.toLowerCase()=='scissors')) {
+        } else {
+        alert('That\'s not a valid choice.');
+        getPlayerInput();
+        }
 
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(computerScore);
-console.log(playerScore);
+        return playerSelection; // return the value entered to () 
+}
+
+/*  Funtion to work out winner
+    added variables to this funtion.  console.log(declareResult(1,1)) passes the scores through to this function block, the values are worked on and then the result is returned.
+*/
+function declareResult(playerScore,computerScore) {       
+    if (playerScore===computerScore) {
+        alert(playerScore + '-' + computerScore + '\nIt\'s a Draw!');
+        return playerScore + '-' + computerScore + '\nIt\'s a Draw!';
+      } else if (playerScore>computerScore) {
+        alert(playerScore + '-' + computerScore + '\nYou win!!');
+        return playerScore + '-' + computerScore + '\nYou win!!';
+      } else {
+        alert(playerScore + '-' + computerScore + '\nYou lose. Better luck next time!');
+        return playerScore + '-' + computerScore + '\nYou lose. Better luck next time!'
+      }
+    }
+
+
+    function playGame () {
+        computerScore = 0;
+        playerScore = 0;
+        for (let i=0; i<5; i++) {
+            getPlayerInput();
+        }
+        console.log(declareResult());
+    }
