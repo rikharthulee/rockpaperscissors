12b816e3887521eca67b80e8a4b2c138991ba74c
rikharhthulee <rikharhthulee@gmail.com>
2023-10-22 02:44:32 +0100

    Simplifed getComputerChoice

--- script.js
+++ script.js
@@ -2,25 +2,17 @@
 
 const options = ['Rock', 'Paper', 'Scissors']; // options in an array
 
-
 function getComputerChoice() {
-
-    // get random number from 0 to total of items in the array.  If more items are added, they will be automatically included.
-    const randomIndex = Math.floor(Math.random() * options.length);
-
-    // get random choice
-    const choice = options[randomIndex]; 
-
-    return choice;
+    return options[~~(Math.random()*options.length)]
 }
 
 const computerSelection = getComputerChoice()
 
 
+
+
+let userSelection = prompt("Rock, Paper or Scissors?"); //ask user for their selection
+
 console.log(computerSelection);
-
-
-let userSelection = prompt("Rock, Paper or Scissors?");
-
 console.log(userSelection)
 
